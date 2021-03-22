80e4f477c4a85f8b46419c541f2e7c9a8d1ee5d3
phil <bhouba@gmail.com>
2021-03-22 08:59:52 +0100

    nouvelles vues evenement

--- evenement/views.py
+++ evenement/views.py
@@ -18,7 +18,6 @@ def liste_evenements(request):
     data = {
         # on filtre les évènements sur ceux de l'asso uniquement
         "Evenements": Evenement.objects.filter(association_id=uuid_asso),
-
     }
     return render(request, "evenement/evenements_liste.html", data)
 
@@ -32,8 +31,11 @@ def detail_evenement(request, uuid_event):
     request.session['uuid_evenement'] = uuid_event
 
     evenement = Evenement.objects.get(UUID_evenement=uuid_event)
+    equipes = Equipe.objects.filter(evenement_id=uuid_event)
     data = {
+
         "Evenement": evenement,
+        "Equipes": equipes,
     }
     return render(request, "evenement/evenement_detail.html", data)
 
@@ -52,7 +54,6 @@ def liste_equipes(request):
     data = {
         # on filtre les équipes sur celles de l'evenement uniquement
         "Equipes": Equipe.objects.filter(evenement_id=uuid_evt),
-
     }
     return render(request, 'evenement/equipes_liste.html', data)
 
@@ -66,9 +67,10 @@ def detail_equipe(request, uuid_equipe):
     request.session['uuid_equipe'] = uuid_equipe
 
     equipe = Equipe.objects.get(UUID_equipe=uuid_equipe)
+    plannings = Planning.objects.filter(equipe_id=uuid_equipe)
     data = {
         "Equipe": equipe,
-
+        "Plannings" : plannings,
     }
     return render(request, 'evenement/equipe_detail.html', data)
 
@@ -87,7 +89,6 @@ def liste_plannings(request):
     data = {
         # on filtre les plannings sur ceux de l'equipe uniquement
         "Plannings": Planning.objects.filter(equipe_id=uuid_equipe),
-
     }
     return render(request, 'evenement/plannings_liste.html', data)
 
@@ -101,9 +102,10 @@ def detail_planning(request, uuid_planning):
     request.session['uuid_planning'] = uuid_planning
 
     planning = Planning.objects.get(UUID_planning=uuid_planning)
+    postes = Poste.objects.filter(planning_id=uuid_planning)
     data = {
         "Planning": planning,
-
+        "Postes": postes,
     }
     return render(request, 'evenement/planning_detail.html', data)
 
@@ -135,9 +137,10 @@ def detail_poste(request, uuid_poste):
     request.session['uuid_poste'] = uuid_poste
 
     poste = Poste.objects.get(UUID_poste=uuid_poste)
+    creneaux = Creneau.objects.filter(poste_id=uuid_poste)
     data = {
         "Poste": poste,
-
+        "Creneaux": creneaux, # pb sur NoReverseMatch
     }
     return render(request, 'evenement/poste_detail.html', data)
 
@@ -156,7 +159,6 @@ def liste_creneaux(request):
     data = {
         # on filtre les postes sur ceux des plannings uniquement
         "Creneaux": Creneau.objects.filter(poste_id=uuid_poste),
-
     }
     return render(request, 'evenement/creneaux_liste.html', data)
 
@@ -172,6 +174,5 @@ def detail_creneau(request, uuid_creneau):
     creneau = Creneau.objects.get(UUID_creneau=uuid_creneau)
     data = {
         "Creneau": creneau,
-
     }
     return render(request, 'evenement/creneau_detail.html', data)
