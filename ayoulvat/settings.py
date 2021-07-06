dfa25185d4fb0f77a75ee5981ab0fa12f6635a4b
phil <bhouba@gmail.com>
2021-07-06 22:40:40 +0200

    static & media test

--- ayoulvat/settings.py
+++ ayoulvat/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.localhost', 'ayoulvat.deusta.bzh']
 
@@ -138,15 +138,16 @@ LANGUAGES = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # gestion des medias
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
-# MEDIA_ROOT = os.path.join(BASE_DIR, "static_media")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # authent personnalisée
 AUTH_USER_MODEL = 'benevole.Personne'
