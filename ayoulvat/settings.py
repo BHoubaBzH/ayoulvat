335522997255e198eb448a1ce651f1693ffdfc4f
phil <bhouba@gmail.com>
2021-07-07 22:05:44 +0200

    allowed hosts

--- ayoulvat/settings.py
+++ ayoulvat/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.localhost', 'ayoulvat.deusta.bzh']
+ALLOWED_HOSTS = ['ayoulvat.deusta.bzh']
 
 # Application definition
 
