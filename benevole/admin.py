8b1646f15f2b4424096451f632570ffe35cdb76d
phil <bhouba@gmail.com>
2021-08-31 10:32:04 +0200

    améliorations diverses CSS

--- benevole/admin.py
+++ benevole/admin.py
@@ -13,7 +13,7 @@ class PersonneInLine(admin.TabularInline):
     extra = 1
 
 class PersonneDetails(admin.ModelAdmin):
-    list_display = ("username", "last_name", "first_name", "is_superuser")
+    list_display = ("username", "last_name", "first_name", "date_joined", "is_superuser")
 
 '''
 class BenevoleDetails(UserAdmin):
