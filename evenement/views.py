a1ab4b165ab3e56f5b5f4198bee056714163c610
phil <bhouba@gmail.com>
2021-09-24 13:34:26 +0200

    logs temps fonctions de la vue evenement

--- evenement/views.py
+++ evenement/views.py
@@ -56,6 +56,7 @@ def planning_range(debut, fin, delta):
         clés  : valeurs
         dates : ( heures, datetimes)
     """
+    print('*** Debut fonction planning_range : {}'.format(datetime.datetime.now()))
     # print('###### planning : {0} - {1}'.format(debut, fin))
     dates_heures = {}
     while debut <= fin:
@@ -64,6 +65,7 @@ def planning_range(debut, fin, delta):
         heure = debut.strftime("%H:%M")
         dates_heures[date] = [heure, debut]
         debut += timedelta(minutes=delta)
+    print('*** Fin fonction planning_range : {}'.format(datetime.datetime.now()))
     return dates_heures
 
 
@@ -94,6 +96,7 @@ def tous_creneaux_entre_2_heures(debut, fin, uuid_evenement):
             creneaux : liste de creneaux
         donne tous les créneaux d'un evenement entre 2 date pour savor si un benevole est deja occupé
     """
+    print('*** Debut fonction tous_creneaux_entre_2_heures : {}'.format(datetime.datetime.now()))
     crenos_out = []  # liste
     crenos = Creneau.objects.filter(evenement_id=uuid_evenement)
     for creno in crenos:
@@ -110,6 +113,7 @@ def tous_creneaux_entre_2_heures(debut, fin, uuid_evenement):
             creno.fin = fin
             crenos_out.append(creno)
         # print(' creno : {}'.format(creno.nom))
+    print('*** Fin fonction tous_creneaux_entre_2_heures : {}'.format(datetime.datetime.now()))
     return crenos_out
 
 
@@ -123,6 +127,7 @@ def forms_equipe(request, data, uuid_evenement):
             null
         gère la création, modification et suppression des equipes en fonction du contenu de POST
     """
+    print('*** Debut fonction forms_equipe : {}'.format(datetime.datetime.now()))
     if any(x in request.POST for x in ['equipe_modifier', 'equipe_ajouter']):
         if 'equipe_modifier' in request.POST:
             formequipe = EquipeForm(request.POST,
@@ -148,6 +153,7 @@ def forms_equipe(request, data, uuid_evenement):
         formequipe = EquipeForm(instance=Equipe.objects.get(UUID=equipe.UUID))
         dic_equipe_init[equipe.UUID] = formequipe  # dictionnaire des forms
         # print (' equipe UUID : {}'.format(equipe.UUID))
+    print('*** Fin fonction forms_equipe : {}'.format(datetime.datetime.now()))
     return dic_equipe_init
 
 def forms_planning(request, data, uuid_evenement):
@@ -160,6 +166,7 @@ def forms_planning(request, data, uuid_evenement):
             null
         gère la création, modification et suppression des plannings en fonction du contenu de POST
     """
+    print('*** Debut fonction forms_planning : {}'.format(datetime.datetime.now()))
     if any(x in request.POST for x in ['planning_modifier', 'planning_ajouter']):
         if 'planning_modifier' in request.POST:
             formplanning = PlanningForm(request.POST,
@@ -185,6 +192,7 @@ def forms_planning(request, data, uuid_evenement):
         formplanning = PlanningForm(instance=Planning.objects.get(UUID=planning.UUID))
         dic_planning_init[planning.UUID] = formplanning  # dictionnaire des forms
         # print (' planning UUID : {}'.format(planning.UUID))
+    print('*** Fin fonction forms_planning : {}'.format(datetime.datetime.now()))
     return dic_planning_init
 
 def forms_postes(request, data, uuid_evenement):
@@ -197,6 +205,7 @@ def forms_postes(request, data, uuid_evenement):
             dictionnaire des forms postes: key: UUID / val: form
         gère la création, modification et suppression de poste en fonction du contenu de POST
     """
+    print('*** Debut fonction forms_postes : {}'.format(datetime.datetime.now()))
     # sauvegarde notre form modifée et crée envoyée en POST
     if any(x in request.POST for x in ['poste_modifier', 'poste_ajouter']):
         # form en lien avec l objet basé sur model et pk UUID poste
@@ -225,6 +234,7 @@ def forms_postes(request, data, uuid_evenement):
         formposte = PosteForm(instance=Poste.objects.get(UUID=poste.UUID))
         dic_postes_init[poste.UUID] = formposte  # dictionnaire des forms
         # print (' poste UUID : {1} form : {0}'.format(formposte, poste.UUID))
+    print('*** Fin fonction forms_postes : {}'.format(datetime.datetime.now()))
     return dic_postes_init
 
 
@@ -238,6 +248,7 @@ def forms_creneaux(request, data, uuid_evenement):
             dictionnaire des forms creneaux: key: UUID / val: form
         gère la création, modification et suppression de creneaux en fonction du contenu de POST
     """
+    print('*** Debut fonction forms_creneaux : {}'.format(datetime.datetime.now()))
     if any(x in request.POST for x in ['creneau_modifier', 'creneau_ajouter']) and not request.POST.get('creneau_supprimer'):
         # form en lien avec l objet basé sur model et pk UUID creneau
         if 'creneau_modifier' in request.POST:
@@ -284,6 +295,7 @@ def forms_creneaux(request, data, uuid_evenement):
                                   type=creneau._meta.get_field('type').value_from_object(creneau), )
         dic_creneaux_init[creneau.UUID] = formcreneau  # dictionnaire des forms: key: UUID / val: form
         # print(' creneau UUID : {1} form : {0}'.format(formcreneau, creneau.UUID))
+    print('*** Fin fonction forms_creneaux : {}'.format(datetime.datetime.now()))
     return dic_creneaux_init
 
 
