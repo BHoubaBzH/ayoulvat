e12eda37ea4f7d1dc417e025c11de79bb6d32b1c
phil <bhouba@gmail.com>
2022-05-10 22:14:31 +0200

    liste benevoles améliorée

--- evenement/templatetags/filters_perso.py
+++ evenement/templatetags/filters_perso.py
@@ -1,4 +1,7 @@
+# ficher présent dans la sidebar, donc dans tout le projet
+
 from django import template
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
@@ -16,3 +19,12 @@ def has_group(user, group_name):
         fonction permettant de trouver si le user appartient a un groupe
     """
     return user.groups.filter(name=group_name).exists() 
+
+@register.filter
+@stringfilter
+def upto(value, delimiter=None):
+    """
+        garde la string jusqu'au paramète donné 
+    """
+    return value.split(delimiter)[0]
+upto.is_safe = True
